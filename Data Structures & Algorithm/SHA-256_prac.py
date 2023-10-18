3fc29e2ed878dc98f9ef473d1aaa6d1314ec9aff
giljihun <accc45@outlook.com>
2023-10-18 21:57:24 +0900

    Save. 23.10.18

--- Data Structures & Algorithm/SHA-256_prac.py
+++ Data Structures & Algorithm/SHA-256_prac.py
@@ -2,9 +2,11 @@ import hashlib
 
 target1 = "giijihun"
 result1 = hashlib.sha256(target1.encode()).hexdigest()
+
 print("Result of Hashing - [giljihun] : ",result1)
 
 target2 = "giijihun!"
 result2 = hashlib.sha256(target2.encode()).hexdigest()
+
 print("Result of Hashing - [giljihun!] : ",result2)
 
